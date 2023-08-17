75053c031c845b1b862441ec30f01d6760e98587
mertkozlu <124254294+mertkozlu@users.noreply.github.com>
2023-08-17 12:50:09 +0300

    E-ShoppingFront-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'E-Shopping-fe';
+  title: string = 'E-Shopping-fe';
+  user: string = 'Mert Kozlu'
+  product1 = {
+    productId: 1,
+    productName: 'Bardak',
+    categoryId: 1,
+    unitPrice: 5
+  };
+  product2 = {
+    productId: 2,
+    productName: 'Mouse',
+    categoryId: 1,
+    unitPrice: 5
+  };
+  product3 = {
+    productId: 3,
+    productName: 'Laptop',
+    categoryId: 1,
+    unitPrice: 5
+  };
+  product4 = {
+    productId: 4,
+    productName: 'Pc',
+    categoryId: 1,
+    unitPrice: 5
+  };
+
+  products = [
+    this.product1, 
+    this.product2, 
+    this.product3, 
+    this.product4
+  ];
+
+
 }
